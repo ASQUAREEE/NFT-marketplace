304c684240b0437d34eafc171f3ddc150168382c
Ashher Ali <ashher868@gmail.com>
2024-04-15 18:02:40 +0530

    create avatar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,63 @@
+import CreatorCard from "@/components/CreatorCard";
 import Banner from "@/components/helper/Banner";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import {useState, useEffect, useRef} from "react";
+import images from "../assets"
+import { makeId } from "@/utils/makeId";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+
+type ImageKey = keyof typeof images;
 
 export default function Home() {
+
+  const [hideButton, setHideButton] = useState(false);
+  
+  const parentRef = useRef<HTMLDivElement | null>(null);
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const {theme} = useTheme();
+  
+  const handleScroll = (direction:string) => {
+
+    const current = scrollRef.current;
+
+  const scrollAmount = window.innerWidth > 1800 ? 270 : 210;
+
+  if (current) {
+    if (direction === "left") {
+      current.scrollLeft -= scrollAmount;
+    } else {
+      current.scrollLeft += scrollAmount;
+    }
+  }
+
+  }
+
+  const isScrollable = () => {
+    const {current} = scrollRef;
+    const {current: parent} = parentRef;
+
+    if (current && parent) {
+      if (current?.scrollWidth >= parent?.offsetWidth) {
+        setHideButton(false);
+      }
+
+     else {
+      setHideButton(true);
+     }
+    }
+  }
+
+
+  useEffect(()=>{
+  isScrollable();
+  window.addEventListener('resize', isScrollable);
+
+  return () => {
+    window.removeEventListener('resize',isScrollable);
+  }
+  })
+
   return (
     <>
   <div className="flex justify-center sm:px-4 p-12">
@@ -17,6 +71,60 @@ export default function Home() {
 
      childStyle="md:text-4xl sm:text-2xl xs:text-xl text-left"
      />
+
+     <div>
+      <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">Best Creators</h1>
+
+      <div className="relative flex-1 max-w-full flex mt-3"
+      ref={parentRef}
+      >
+       <div className="flex flex-row w-max overflow-x-scroll no-scrollbar select-none"
+       ref={scrollRef}
+       >
+
+       {
+        [6,7,8,9,10].map((i)=> (
+          <CreatorCard 
+          key={`creator-${i}`}
+          rank={i}
+          creatorImage={images[`creator${i}` as ImageKey]}
+          creatorName={`0x${makeId(3)}....${makeId(3)}`}
+          creatorEths={10- i*0.5}
+          />
+        ))
+       }
+
+       {!hideButton && <>
+       <div
+       onClick={()=>{handleScroll('left')}}
+       className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer left-0">
+        <Image 
+        src={images.left}
+        alt='arrow left'
+        layout="fill"
+        objectFit="contain"
+        className={theme === 'light' ? "filter invert" :""}
+        />
+       </div>
+
+       <div
+       onClick={()=>{handleScroll('right')}}
+       className="absolute w-8 h-8 minlg:w-12 minlg:h-12 top-45 cursor-pointer right-0">
+        <Image 
+        src={images.right}
+        alt='arrow left'
+        layout="fill"
+        objectFit="contain"
+        className={theme === 'light' ? "filter invert" :""}
+        />
+       </div>
+       </>}
+
+        </div>   
+
+      </div>
+
+     </div>
 
     </div>
 
---
+++ utils/makeId.ts
@@ -0,0 +1,15 @@
+export const makeId = (length: number) =>{
+
+let result ="";
+
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+const charactersLength = characters.length
+
+for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength ));
+}
+
+return result
+
+}
