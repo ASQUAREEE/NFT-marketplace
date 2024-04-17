f392bf1f68dcee7dc390ac5b505e125495fae1cd
Ashher Ali <ashher868@gmail.com>
2024-04-17 17:29:16 +0530

    entire homepage created

---
+++ components/NFTCard.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import images from '../assets'
+import Link from 'next/link';
+import Image from 'next/image';
+
+// Define the interface for the NFT object
+type NFT = {
+  i: number;  
+  name: string;
+  seller: string;
+  owner: string;
+  description: string;
+  price: string;
+}
+
+// Define the props interface for the NFTCard component
+type NFTCardProps = {
+  nft: NFT;
+}
+
+const NFTCard: React.FC<NFTCardProps> = ({ nft }) => {
+
+  const { i, name, seller, owner, description, price } = nft;
+
+  const nftImageKey = `nft${i}` as keyof typeof images;
+  const nftImage = images[nftImageKey];
+
+  return (
+    
+     <Link href={{ pathname: '/nft-details', query:nft}}>
+     
+
+      <div className='flex-1 min-w-215 max-w-max xs:max-w-none sm:w-full sm:min-w-155 minmd:min-w-256 minlg:min-w-327 dark:bg-nft-black-3 bg-white rounded-2xl p-4 m-4 minlg:m-8 sm:my-2 sm:mx-2 cursor-pointer shadow-md'>       
+
+    <div className="relative w-full h-52 sm:h-36 xs:h-56 minmd:h-60 minlg:h-300 rounded-2xl overflow-hidden">
+        <Image
+        layout='fill'
+        objectFit='cover'
+        src={nftImage}
+        alt={`nft${i}`}
+
+        />
+    </div>
+       
+       <div className='mt-3 flex flex-col'>
+
+        <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">{name}</p>
+
+
+        <div className='flexBetween mt-1 minlg:mt-3 fllex-row xs:flex-col xs:items-start xs:mt-3'>
+            <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xs minlg:text-lg">{price} <span className="normal">ETH</span></p>
+
+            <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xs minlg:text-lg">{seller}</p>
+        </div>
+
+       </div>
+
+      </div>
+
+ 
+
+     </Link>
+
+
+  );
+};
+
+export default NFTCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import images from "../assets"
 import { makeId } from "@/utils/makeId";
 import Image from "next/image";
 import { useTheme } from "next-themes";
+import NFTCard from "@/components/NFTCard";
 
 type ImageKey = keyof typeof images;
 
@@ -88,7 +89,7 @@ export default function Home() {
           key={`creator-${i}`}
           rank={i}
           creatorImage={images[`creator${i}` as ImageKey]}
-          creatorName={`0x${makeId(3)}....${makeId(3)}`}
+          creatorName={`0x${makeId(3)}....${makeId(4)}`}
           creatorEths={10- i*0.5}
           />
         ))
@@ -123,6 +124,40 @@ export default function Home() {
         </div>   
 
       </div>
+
+     </div>
+
+     <div className="mt-10">
+         <div className="flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start">
+
+         <h1 className="flex-1 font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4">Hot Bids</h1>
+
+         <div>
+          SearchBar
+         </div>
+
+         </div>
+
+        <div className="mt-3 w-full flex flex-wrap justify-start md:justify-center">
+
+        {[1,2,3,4,5,6,7,8,9,10].map((i)=> (
+          <NFTCard
+          key={`nft-${i}`} 
+          nft= {{   
+            i,
+            name: `Nifty NFT ${i}`,
+            seller: `0x${makeId(3)}....${makeId(4)}`,
+            owner: `0x${makeId(3)}....${makeId(4)}`,
+            description: "cool NFT on Sale",
+            price: (10- i*0.534).toFixed(2),
+
+          }} 
+
+              />
+        ))}  
+
+        </div>
+        
 
      </div>
 
